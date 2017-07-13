1bb6ee978d0985acc0cae738247684d98ac11beb
jonimoas <setpsistariasbaniou@gmail.com>
2017-07-14 01:10:34 +0300

    first upload

---
+++ Module1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x, y, total, z, out, ins, o, d, pi As Double
+        z = Console.ReadLine
+        x = -1
+        y = -1
+        total = 0
+        o = (1 / z)
+        Do While x <= 1
+            Do While y <= 1
+                d = System.Math.Sqrt(x ^ 2 + y ^ 2)
+                If d >= 1 Then
+                    out = out + 1
+                Else
+                    ins = ins + 1
+                End If
+                total = total + 1
+                y = y + o
+                Console.Title = "(" & x & " , " & y & ")"
+            Loop
+            x = x + o
+            y = -1
+            Console.Title = "(" & x & " , " & y & ")"
+        Loop
+        pi = (ins / (ins + out)) * 4
+        Console.Out.WriteLine(pi)
+        Console.Out.WriteLine(total)
+        Stop
+    End Sub
+
+End Module
---
+++ README.md
@@ -0,0 +1,6 @@
+Part of the collection of the programs i started writing when i was 13 years old.
+
+This application attempts to calculate pi by breaking the trigonometric circle in as many points
+as you request and checking if said point is inside or outside the circle. 
+
+It does not end correctly but i decided to side with my 15 yo self :)
